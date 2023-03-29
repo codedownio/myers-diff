31d11c62a2794343741bec16e293c22aa7deb204
Tom McLaughlin <tom@codedown.io>
2023-03-29 10:18:21 -0700

    Current VectorMyers tests passing

--- src/Data/Diff/MyersShim.hs
+++ src/Data/Diff/MyersShim.hs
@@ -6,55 +6,69 @@ module Data.Diff.MyersShim (
 import Data.Diff.Types
 import Data.Function
 import Data.Sequence
+import qualified Data.Text as T
 import Data.Vector.Unboxed as VU
 import Prelude hiding (read)
 
+import Data.String.Interpolate
+import Debug.Trace
+
 
 editScriptToChangeEvents :: VU.Vector Char -> VU.Vector Char -> Seq Edit -> Seq ChangeEvent
-editScriptToChangeEvents left right = go mempty 0 0 0 0
+editScriptToChangeEvents left right = go mempty 0 0 0
   where
-    go :: Seq ChangeEvent -> Int -> Int -> Int -> Int -> Seq Edit -> Seq ChangeEvent
-    go seqSoFar _ _ _ _ Empty = seqSoFar
+    go :: Seq ChangeEvent -> Int -> Int -> Int -> Seq Edit -> Seq ChangeEvent
+    go seqSoFar _ _ _ Empty = seqSoFar
 
-    go seqSoFar lastPos curLine curCh offset ((EditDelete ((\x -> x - offset) -> from) ((\x -> x - offset) -> to)) :<| rest) = go (seqSoFar |> newChange) newPos newLine newCh newOffset rest
+    -- Implicit unchanged section before delete
+    go seqSoFar pos line ch args@((EditDelete from to) :<| _) |
+      pos < from = go seqSoFar from line' ch' args
+        where
+          (numNewlinesEncountered, lastLineLength) = countNewlinesAndLastLineLength (VU.slice pos (from - pos) left)
+          line' = line + numNewlinesEncountered
+          ch' | numNewlinesEncountered == 0 = ch + (from - pos)
+              | otherwise = lastLineLength
+    -- Implicit unchanged section before insert
+    go seqSoFar pos line ch args@((EditInsert from rightFrom rightTo) :<| _) |
+      pos < from = go seqSoFar from line' ch' args
+        where
+          (numNewlinesEncountered, lastLineLength) = countNewlinesAndLastLineLength (VU.slice pos (from - pos) left)
+          line' = line + numNewlinesEncountered
+          ch' | numNewlinesEncountered == 0 = ch + (from - pos)
+              | otherwise = lastLineLength
+
+    go seqSoFar pos line ch ((EditDelete from to) :<| rest) = go (seqSoFar |> change) pos' line' ch' rest
       where
-        newChange = ChangeEvent range ""
-        range = Range (Position l1 c1) (Position l2 c2)
-
-        unchangedNewlines = countNewlines (VU.slice lastPos (from - lastPos) left)
-
-        l1 = curLine + unchangedNewlines
-        c1 = if | unchangedNewlines == 0 -> curCh + (from - lastPos)
-                | otherwise -> lengthOfLastLine (VU.slice lastPos (from - lastPos) left)
+        change = ChangeEvent (Range (Position line ch) (Position line' ch')) ""
+        pos' = pos + (to - from) + 1
 
         deleted = VU.slice from (to - from) left
-        numNewlinesInDeleted = countNewlines deleted
+        (numNewlinesInDeleted, lastLineLengthInDeleted) = countNewlinesAndLastLineLength deleted
+        line' = line + numNewlinesInDeleted
+        ch' = if | numNewlinesInDeleted == 0 -> ch + (to - pos + 1)
+                 | otherwise -> lastLineLengthInDeleted
 
-        l2 = l1 + numNewlinesInDeleted
-        c2 = if | numNewlinesInDeleted == 0 -> curCh + (to + 1 - lastPos)
-                | otherwise -> 43
-
-        newPos = lastPos
-        newLine = curLine
-        newCh = curCh
-        newOffset = offset + to - from + 1
-
-    go seqSoFar lastPos curLine curCh offset ((EditInsert at from to) :<| rest) = go (seqSoFar |> newChange) newPos newLine newCh newOffset rest
+    go seqSoFar pos line ch ((EditInsert at rightFrom rightTo) :<| rest) = go (seqSoFar |> change) pos' line' ch' rest
       where
-        newOffset = undefined
-        newChange = undefined
-        newPos = undefined
-        newLine = undefined
-        newCh = undefined
+        change = ChangeEvent (Range (Position line ch) (Position line ch)) (vectorToText (VU.slice rightFrom (rightTo - rightFrom) right))
+        pos' = pos
+        line' = line
+        ch' = ch
 
 
 
-countNewlines :: VU.Vector Char -> Int
-countNewlines = foldl' (\tot ch -> if ch == '\n' then tot + 1 else tot) 0
+-- countNewlines :: VU.Vector Char -> Int
+-- countNewlines = foldl' (\tot ch -> if ch == '\n' then tot + 1 else tot) 0
 
-lengthOfLastLine :: Vector Char -> Int
-lengthOfLastLine vec = flip fix (0, VU.length vec - 1) $ \loop -> \case
-  (tot, i)
-    | i < 0 -> tot
-    | vec `unsafeIndex` i == '\n' -> tot
-    | otherwise -> loop (tot + 1, i - 1)
+countNewlinesAndLastLineLength :: VU.Vector Char -> (Int, Int)
+countNewlinesAndLastLineLength = foldl' (\(tot, lastLineLength) ch -> if ch == '\n' then (tot + 1, 0) else (tot, lastLineLength + 1)) (0, 0)
+
+vectorToText :: VU.Vector Char -> T.Text
+vectorToText = T.pack . VU.toList
+
+-- lengthOfLastLine :: Vector Char -> Int
+-- lengthOfLastLine vec = flip fix (0, VU.length vec - 1) $ \loop -> \case
+--   (tot, i)
+--     | i < 0 -> tot
+--     | vec `unsafeIndex` i == '\n' -> tot
+--     | otherwise -> loop (tot + 1, i - 1)
