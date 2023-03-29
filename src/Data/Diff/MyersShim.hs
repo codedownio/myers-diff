624243bf6347e69a4300671e7616d717202b3fde
Tom McLaughlin <tom@codedown.io>
2023-03-29 02:40:32 -0700

    Specialize editScriptToChangeEvents to Vector Char

--- src/Data/Diff/MyersShim.hs
+++ src/Data/Diff/MyersShim.hs
@@ -9,19 +9,21 @@ import Data.Vector.Unboxed as VU
 import Prelude hiding (read)
 
 
-editScriptToChangeEvents :: VU.Vector a -> VU.Vector a -> Seq Edit -> Seq ChangeEvent
-editScriptToChangeEvents left right = go mempty 0
+editScriptToChangeEvents :: VU.Vector Char -> VU.Vector Char -> Seq Edit -> Seq ChangeEvent
+editScriptToChangeEvents left right = go mempty 0 0
   where
-    go :: Seq ChangeEvent -> Int -> Seq Edit -> Seq ChangeEvent
-    go seqSoFar _pos Empty = seqSoFar
+    go :: Seq ChangeEvent -> Int -> Int -> Seq Edit -> Seq ChangeEvent
+    go seqSoFar _curLine _curCh Empty = seqSoFar
 
-    go seqSoFar pos ((EditDelete from to) :<| rest) = go (seqSoFar |> newChange) newPos rest
+    go seqSoFar curLine curCh ((EditDelete from to) :<| rest) = go (seqSoFar |> newChange) newLine newCh rest
       where
         newChange = ChangeEvent range ""
         range = undefined
-        newPos = undefined
+        newLine = undefined
+        newCh = undefined
 
-    go seqSoFar pos ((EditInsert at from to) :<| rest) = go (seqSoFar |> newChange) newPos rest
+    go seqSoFar curLine curCh ((EditInsert at from to) :<| rest) = go (seqSoFar |> newChange) newLine newCh rest
       where
         newChange = undefined
-        newPos = undefined
+        newLine = undefined
+        newCh = undefined
