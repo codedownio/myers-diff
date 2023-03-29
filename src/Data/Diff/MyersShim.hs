d0e4651fd056242d1f3532e9335a9614232c5731
Tom McLaughlin <tom@codedown.io>
2023-03-29 03:50:41 -0700

    Starting to work on editScriptToChangeEvents

--- src/Data/Diff/MyersShim.hs
+++ src/Data/Diff/MyersShim.hs
@@ -10,20 +10,37 @@ import Prelude hiding (read)
 
 
 editScriptToChangeEvents :: VU.Vector Char -> VU.Vector Char -> Seq Edit -> Seq ChangeEvent
-editScriptToChangeEvents left right = go mempty 0 0
+editScriptToChangeEvents left right = go mempty 0 0 0
   where
-    go :: Seq ChangeEvent -> Int -> Int -> Seq Edit -> Seq ChangeEvent
-    go seqSoFar _curLine _curCh Empty = seqSoFar
+    go :: Seq ChangeEvent -> Int -> Int -> Int -> Seq Edit -> Seq ChangeEvent
+    go seqSoFar _lastPos _curLine _curCh Empty = seqSoFar
 
-    go seqSoFar curLine curCh ((EditDelete from to) :<| rest) = go (seqSoFar |> newChange) newLine newCh rest
+    go seqSoFar lastPos curLine curCh ((EditDelete from to) :<| rest) = go (seqSoFar |> newChange) newPos newLine newCh rest
       where
         newChange = ChangeEvent range ""
-        range = undefined
+        range = Range (Position l1 c1) (Position l2 c2)
+
+        unchangedNewlines = countNewlines (VU.slice lastPos from left)
+
+        l1 = curLine + unchangedNewlines
+        c1 = if | unchangedNewlines == 0 -> curCh + (from - lastPos)
+                | otherwise -> undefined -- then lengthOfLastNewlineInRange vec lastPos from
+
+        l2 = l1 + countNewlines (VU.slice from to left)
+        c2 = undefined
+
+        newPos = lastPos
+        newLine = curLine
+        newCh = curCh
+
+    go seqSoFar lastPos curLine curCh ((EditInsert at from to) :<| rest) = go (seqSoFar |> newChange) newPos newLine newCh rest
+      where
+        newChange = undefined
+        newPos = undefined
         newLine = undefined
         newCh = undefined
 
-    go seqSoFar curLine curCh ((EditInsert at from to) :<| rest) = go (seqSoFar |> newChange) newLine newCh rest
-      where
-        newChange = undefined
-        newLine = undefined
-        newCh = undefined
+
+
+countNewlines :: VU.Vector Char -> Int
+countNewlines = foldl' (\tot ch -> if ch == '\n' then tot + 1 else tot) 0
