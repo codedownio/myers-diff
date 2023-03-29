85fca95ce00e068ef71ad6bd3ca46828fe7b6107
Tom McLaughlin <tom@codedown.io>
2023-03-29 12:05:00 -0700

    Try filling in more benchmarking

--- bench/Main.hs
+++ bench/Main.hs
@@ -9,9 +9,11 @@ import Control.DeepSeq
 import Criterion
 import Criterion.Main
 import Data.Diff.Types
+import Data.Text as T
 import GHC.Generics
 
 import qualified Data.Diff.DiffMyersShim as DM
+import qualified Data.Diff.VectorMyers as VM
 
 
 deriving instance Generic Position
@@ -23,13 +25,16 @@ deriving instance NFData Range
 deriving instance Generic ChangeEvent
 deriving instance NFData ChangeEvent
 
-getPair :: IO (String, String)
+getPair :: IO (String, String, Text, Text)
 getPair = do
   putStrLn "Generating pair"
-  return ("abc", "abcd")
+  return ("abc", "abcd", T.pack "abc", T.pack "abcd")
 
 main :: IO ()
 main = defaultMain [
-  env getPair $ \(~(initial, final)) -> do
-    bench "Simple" $ nf (\(x, y) -> DM.diffDiff x y) (initial, final)
+  env getPair $ \(~(initial, final, initialText, finalText)) ->
+    bgroup "Simple" [
+      bench "Diff" $ nf (\(x, y) -> DM.diffDiff x y) (initial, final)
+      , bench "Vector" $ nf (\(x, y) -> VM.diffTextsToChangeEvents x y) (initialText, finalText)
+    ]
   ]
