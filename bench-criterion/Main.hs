0e51ec9020cce4113559a83cc324a09975f2c8fd
Tom McLaughlin <tom@codedown.io>
2023-10-26 19:54:02 -0700

    More on benchmarking

--- bench-criterion/Main.hs
+++ bench-criterion/Main.hs
@@ -2,9 +2,11 @@
 
 module Main (main) where
 
+import Control.Monad
 import Criterion
 import Criterion.Main
 import qualified Data.Diff.Myers as VM
+import qualified Data.List as L
 import Data.String.Interpolate
 import Data.Text as T
 import Test.QuickCheck
@@ -16,27 +18,44 @@ import qualified Data.Diff.Diff as DD
 #endif
 
 
-getPair :: Int -> IO (String, String, Text, Text)
-getPair size = do
-  putStrLn [i|Generating pair of size #{size}|]
-  (t1, t2) <- generate ((resize size arbitraryAlphanumericString) >>= (\initial -> (initial, ) <$> arbitraryInsertOn initial))
-  putStrLn [i|#{t1} vs #{t2}|]
-  return (T.unpack t1, T.unpack t2, t1, t2)
+type PairFn = Int -> Int -> IO [(String, String, Text, Text)]
+
+testGroup :: PairFn -> Int -> Int -> Benchmark
+testGroup getPair numSamples inputSize =
+  env (getPair numSamples inputSize) $ \samples -> do
+    bgroup [i|#{inputSize} characters|] [
+      bench "myers-diff" $ nf (L.map (\(_, _, initialText, finalText) -> (VM.diffTexts initialText finalText))) samples
+#ifdef DIFF
+      , bench "Diff" $ nf (L.map (\(initialString, finalString, _, _) -> (DD.diff initialString finalString))) samples
+#endif
+      ]
+
+getPairWithEdit :: (Text -> Gen Text) -> Int -> Int -> IO [(String, String, Text, Text)]
+getPairWithEdit makeEdit numSamples inputSize = do
+  replicateM numSamples $ do
+    (t1, t2) <- generate ((resize inputSize arbitraryAlphanumericString) >>= (\initial -> (initial, ) <$> makeEdit initial))
+    return (T.unpack t1, T.unpack t2, t1, t2)
+
+getPairSingleInsert :: PairFn
+getPairSingleInsert = getPairWithEdit arbitraryInsertOn
+
+getPairSingleDelete :: PairFn
+getPairSingleDelete = getPairWithEdit arbitraryDeleteOn
 
 main :: IO ()
 main = defaultMain [
-  env (getPair 1000) $ \(~(initial, final, initialText, finalText)) ->
-    bgroup "Diff" [
-      bench "myers-diff" $ nf (uncurry VM.diffTexts) (initialText, finalText)
-
-#ifdef DIFF
-      , bench "Diff" $ nf (\(x, y) -> DD.diff x y) (initial, final)
-#endif
-
-
-      -- , bench "Vector to edit script (consolidated)" $ nf (\(x, y) -> VM.consolidateEditScript $ VM.diffTexts x y) (initialText, finalText)
-      -- , bench "Vector to ChangeEvent" $ nf (\(x, y) -> VM.diffTextsToChangeEvents x y) (initialText, finalText)
-      -- , bench "Vector to ChangeEvents (consolidated)" $ nf (\(x, y) -> VM.diffTextsToChangeEventsConsolidate x y) (initialText, finalText)
-
-    ]
+  bgroup [i|Single insert (100 samples each)|] [
+             testGroup getPairSingleInsert 100 10
+             , testGroup getPairSingleInsert 100 100
+             , testGroup getPairSingleInsert 100 1000
+             , testGroup getPairSingleInsert 100 10000
+             , testGroup getPairSingleInsert 100 100000
+             ]
+  , bgroup [i|Single delete (100 samples each)|] [
+             testGroup getPairSingleDelete 100 10
+             , testGroup getPairSingleDelete 100 100
+             , testGroup getPairSingleDelete 100 1000
+             , testGroup getPairSingleDelete 100 10000
+             , testGroup getPairSingleDelete 100 100000
+             ]
   ]
